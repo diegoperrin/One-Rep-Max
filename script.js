b0f774907700edfa0904da1f350f28f6bd3a3ce6
diegoperrin <dperrinu@gmail.com>
2022-09-06 17:52:41 -0500

    added modal

--- script.js
+++ script.js
@@ -1,11 +1,36 @@
 const btn = document.getElementById("enter")
-const weight= document.getElementById("weight").value
-console.log(weight)
-function Calculate(){
-    
-    
-    alert(weight);
+const weight = document.getElementById("weight")
+const units = document.getElementById("units").value
+const repnum = document.getElementById("output")
+const slider = document.getElementById("reps").oninput = function(){
+    const value = (this.value-this.min)/(this.max)*100
+    repnum.innerHTML = this.value
 
+}
+
+const modal = document.querySelector('.modal');
+const openModal = document.querySelector('.open-button');
+const closeModal = document.querySelector('.close-button');
+
+openModal.addEventListener('click', () => {
+    modal.showModal();
+
+} )
+
+closeModal.addEventListener('click', () => {
+    modal.close();
+
+} )
+
+function Calculate(){
+    let wei = weight.value;
+    let reps = repnum.innerHTML
+
+/* 
+    alert(wei)
+    alert(units)
+    alert(repnum.innerHTML)
+*/
 }
 
 btn.addEventListener('click', Calculate)
