93f1ac6d33844c39b5c02008e84d9acbb11a70bf
diegoperrin <dperrinu@gmail.com>
2022-09-06 15:57:17 -0500

    adding functionality

---
+++ script.js
@@ -0,0 +1,11 @@
+const btn = document.getElementById("enter")
+const weight= document.getElementById("weight").value
+console.log(weight)
+function Calculate(){
+    
+    
+    alert(weight);
+
+}
+
+btn.addEventListener('click', Calculate)
